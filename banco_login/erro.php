34745f25f91f56a79f4965ce920090389bc3a2b7
FELIPE DA SILVA GARCIA <garcia.f@alunoescolassociais.org.br>
2021-11-05 17:09:44 -0300

    atualizaçoes php e começo de css tela login

--- banco_login/erro.php
+++ banco_login/erro.php
@@ -1,9 +1,1 @@
-<!DOCTYPE html>
-<head>
-    
-    <title>Pagina de Erro</title>
-</head>
-<body>
-    <h1>ERRO:Você nao tem cadastro
-</body>
-</html>
+oiiiiii
--- index.php
+++ index.php
@@ -4,9 +4,11 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="banco_login/style.css">
     <title>Login</title>
 </head>
 <body>
+    <div id="corpo-form">
     <h1>Logar</h1>
     <form method="POST" action="banco_login/processa.php"> 
         <label>Usuario:</label>
@@ -19,7 +21,7 @@
         </br>
         </br>
         <input type="submit" value="Logar">
-
+    </div>  
     </form>
     
 </body>
