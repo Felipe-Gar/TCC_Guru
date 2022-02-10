c3235ebfc3accb7720efb38d11ddad1c64eee9ca
FELIPE DA SILVA GARCIA <garcia.f@alunoescolassociais.org.br>
2022-02-10 17:10:51 -0300

    resolução do cadastros

--- AreaPrivada/Colaborador.php
+++ AreaPrivada/Colaborador.php
@@ -4,20 +4,15 @@ include_once("../PoderAdm/usuario_prod.php");
 
 session_start();//abre a sessão 
 
-
-$p = new Produto("system_guru", "localhost", "root", "");
-
-$dados = $p->buscar_dados();
- if(count($dados) > 0)
- {
-     for($i=0; $i < count($dados) ; $i++){
-         foreach ($dados[$i] as $k => $v) {
-             if($k != "id_produto");
-         }
-
-     }
+$result_usu = "SELECT * FROM produto";
+$resultado_usu = mysqli_query($conn, $result_usu);
+ while($row_usuario = mysqli_fetch_assoc($resultado_usu)){
+     echo "Caixa:" . $row_usuario ['numero_caixa'] ;
+     echo "    Nome:" . $row_usuario ['nome'] ;
+     echo "Descrição:" . $row_usuario['descricao'];
+     echo "    Quantidade:" . $row_usuario ['quant_estoque'] ;
+     echo"<a href='../PoderAdm/Editar.php?id_produto=$row_usuario[id_produto]'><button>Emprestar</button></a>";
+     echo"<hr>";
  }
- 
 
- 
 ?>
